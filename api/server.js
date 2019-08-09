b96bfd19fd59644c1b0a49c8f0e749c9a72c1014
Shane Gooch <shanegooch@Shanes-iMac.local>
2019-08-08 17:32:38 -0700

    added cors

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
 
 const server = express();
 
 server.use(helmet());
+server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
